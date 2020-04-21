618817196a9b12e092a1274e94785c1d37c31968
Aigar Sild <aigar.sild@vaimo.com>
2020-04-21 12:27:20 +0300

    Added mobile nav, scrolltotop

--- src/templates/pages/header/header.jsx
+++ src/templates/pages/header/header.jsx
@@ -1,5 +1,7 @@
-import React ,{ Component } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import ScrollToTop from '../../../components/ScrollToTop'
+
 import About from '../about';
 import Home from '../home';
 import Contact from '../contact';
@@ -7,6 +9,22 @@ import Portfolio from '../portfolio';
 import Success from '../success';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showNav: false
+        }
+
+    }
+
+    handleClick(e) {
+        this.setState({
+            showNav: !this.state.showNav
+        });
+
+    }
 
     render() {
         return (
@@ -16,43 +34,50 @@ class Header extends Component {
                     <div className="container">
                         <div className="row">
                             <Link to="/" className="col navbar-brand pb-0 font-weight-bold">Kauplusrattapood</Link>
-                            <nav className="col nav-wrap">
-                                <ul className="nav justify-content-end">
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/">Pealeht</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/about">Meist</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/portfolio">Hinnakiri</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to="/contact">Kontakt</Link>
-                                    </li>
-                                </ul>
-                            </nav>
+                            <div onClick={this.handleClick.bind(this)} className={this.state.showNav ? 'active hamburger' : 'hamburger'}>
+                                <div className="hamburger-icon"></div>
+                            </div>
+                            
+                                <nav className={this.state.showNav ? 'show-nav' : 'col nav-wrap'}>
+                                    <ul className="nav justify-content-end">
+                                        <li className="nav-item">
+                                            <Link onClick={this.handleClick.bind(this)}  className="nav-link" to="/">Pealeht</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link onClick={this.handleClick.bind(this)} className="nav-link" to="/about">Meist</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link onClick={this.handleClick.bind(this)} className="nav-link" to="/portfolio">Hinnakiri</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link onClick={this.handleClick.bind(this)} className="nav-link" to="/contact">Kontakt</Link>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            
                         </div>
                     </div>
                 </header>
 
+                <ScrollToTop>
                 <Switch>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="/portfolio">
-                            <Portfolio />
-                        </Route>
-                        <Route path="/contact">
-                            <Contact />
-                        </Route>
-                        <Route path="/success">
-                            <Success />
-                        </Route>
-                        <Route path="/">
-                            <Home />
-                        </Route>
-                    </Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/portfolio">
+                        <Portfolio />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                    <Route path="/success">
+                        <Success />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+                </ScrollToTop>
             </Router>
         );
     }
